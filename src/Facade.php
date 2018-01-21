90c7f75bca65f9c0489a60723c645316d101b78c
Sinevia <info@sinevia.com>
2018-01-21 10:50:59 +0000

    Create Facade.php

---
+++ src/Facade.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sinevia\Meta;
+
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'meta';
+    }
+}
