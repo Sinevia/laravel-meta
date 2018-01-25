eec9ae8ccee2f26cf8e2d7b77d3555b3baf8a6f2
Sinevia <info@sinevia.com>
2018-01-25 22:36:26 +0000

    Update HasMeta.php

--- src/Models/HasMeta.php
+++ src/Models/HasMeta.php
@@ -8,6 +8,12 @@ trait HasMetas {
         return Metas::get($this->Id, $key, $default);
     }
 
+    /**
+     * Sets key-value pair
+     * @param string $key
+     * @param any $value
+     * @return boolean
+     */
     public function setMeta($key, $value) {
         return Metas::set($this->Id, $key, $value);
     }
